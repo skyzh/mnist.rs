5c0e6c4c70262939e8f9f9230da09cf8bafd7df4
SkyZH <iskyzh@gmail.com>
2019-01-12 21:45:03 +0800

    fix intialization bug and complete training process

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,20 @@
 extern crate rulinalg;
 
 mod trainer;
-use self::trainer::{BatchTrainer, Dataset, Evaluator};
+use self::trainer::{Dataset, Evaluator, Network};
 
 fn main() {
     let dataset = Dataset::new();
-    let mut trainer = BatchTrainer::new();
+    let mut network = Network::init(&vec![784, 30, 10]);
     for epoch in 0..1000 {
-        trainer.train(
+        network.train(
             dataset.trn_size,
             &dataset.get_trn_mat(),
             &dataset.data.trn_lbl,
             20,
-            1.0,
+            3.0,
         );
-        let (correct, total) = Evaluator::evaluate(&dataset, trainer.network());
+        let (correct, total) = Evaluator::evaluate(&dataset, &network);
         println!("Epoch {}: {}/{}", epoch, correct, total);
     }
 }
--- src/trainer/network.rs
+++ src/trainer/network.rs
@@ -28,12 +28,12 @@ impl Network {
         Self {
             layers: layers.clone(),
             biases: (1..layers.len())
-                .map(|idx| Vector::<f64>::from_fn(layers[idx], |_| rand::thread_rng().gen::<f64>()))
+                .map(|idx| Vector::<f64>::from_fn(layers[idx], |_| rand::thread_rng().gen_range(-1.0 as f64, 1.0 as f64)))
                 .collect(),
             weights: (0..(layers.len() - 1))
                 .map(|idx| {
                     Matrix::<f64>::from_fn(layers[idx + 1], layers[idx], |_, _| {
-                        rand::thread_rng().gen::<f64>()
+                        rand::thread_rng().gen_range(-1.0 as f64, 1.0 as f64)
                     })
                 })
                 .collect(),
@@ -48,13 +48,14 @@ impl Network {
         output
     }
 
-    pub fn delta(&mut self, input: &Vector<f64>, output: &Vector<f64>, network: &Network) {
+    pub fn backprop(&mut self, input: &Vector<f64>, output: &Vector<f64>) -> Self {
+        let mut delta_network = Self::new(&self.layers);
         let mut activations = Vec::<Vector<f64>>::new();
         let mut activation_xs = Vec::<Vector<f64>>::new();
         activations.push(input.clone());
 
         for idx in 0..self.biases.len() {
-            let activation = &network.weights[idx] * &activations[idx] + &network.biases[idx];
+            let activation = &self.weights[idx] * &activations[idx] + &self.biases[idx];
             activations.push(activation.clone().apply(&|x| Self::sigmoid(x)));
             activation_xs.push(activation);
         }
@@ -69,38 +70,60 @@ impl Network {
 
         let second_to_last_activation = activations[activations.len() - 2].clone();
         let second_to_last_activation = Self::transpose(second_to_last_activation);
-        *self.weights.last_mut().unwrap() = Self::to_mat(delta.clone()) * second_to_last_activation;
-        *self.biases.last_mut().unwrap() = delta.clone();
+        *delta_network.weights.last_mut().unwrap() =
+            Self::to_mat(delta.clone()) * second_to_last_activation;
+        *delta_network.biases.last_mut().unwrap() = delta.clone();
 
         for idx in (0..self.biases.len() - 1).rev() {
             let sp = (&activation_xs[idx]).clone().apply(&|x| Self::d_sigmoid(x));
-            delta = (network.weights[idx + 1].transpose() * delta).elemul(&sp);
-            self.weights[idx] =
+            delta = (self.weights[idx + 1].transpose() * delta).elemul(&sp);
+            delta_network.weights[idx] =
                 Self::to_mat(delta.clone()) * Self::transpose(activations[idx].clone());
-            self.biases[idx] = delta.clone();
+            delta_network.biases[idx] = delta.clone();
+        }
+        delta_network
+    }
+    pub fn train(
+        &mut self,
+        trn_size: u32,
+        trn_image: &Matrix<f64>,
+        trn_label: &Vec<u8>,
+        batch_size: u32,
+        learning_rate: f64,
+    ) {
+        let mut shuffle_batch: Vec<u32> = (0..trn_size).collect();
+        rand::thread_rng().shuffle(&mut shuffle_batch);
+
+        for i in 0..trn_size / batch_size {
+            let mut nabla_network = Self::new(&self.layers);
+            for j in 0..batch_size {
+                let batch_id = shuffle_batch[(i * batch_size + j) as usize];
+                let row_begin = batch_id * 28;
+                let batch = trn_image.select_rows(
+                    &(row_begin as usize..(row_begin + 28) as usize).collect::<Vec<_>>(),
+                );
+                let batch = Vector::new(batch.iter().map(|v| *v).collect::<Vec<f64>>());
+                let result_vec = Vector::from_fn(10, |idx| {
+                    if trn_label[batch_id as usize] as u32 == idx as u32 {
+                        1.0
+                    } else {
+                        0.0
+                    }
+                });
+                let delta_network = self.backprop(&batch, &result_vec);
+                nabla_network.apply(delta_network, 1.0);
+            }
+            self.apply(nabla_network, -learning_rate / batch_size as f64);
         }
     }
 
-    pub fn apply(&mut self, network: Network, learning_rate: f64) {
-        for (bias, delta_bias) in self.biases.iter_mut().zip(network.biases.iter()) {
-            *bias += delta_bias * (-learning_rate);
+    fn apply(&mut self, network: Network, multiplexer: f64) {
+        for i in 0..self.biases.len() {
+            self.biases[i] += &network.biases[i] * multiplexer;
         }
-        for (weight, delta_weight) in self.weights.iter_mut().zip(network.weights.iter()) {
-            *weight += delta_weight * (-learning_rate);
+        for i in 0..self.weights.len() {
+            self.weights[i] += &network.weights[i] * multiplexer;
         }
-    }
-
-    pub fn sum(&mut self, network: Network) {
-        for (bias, delta_bias) in self.biases.iter_mut().zip(network.biases.iter()) {
-            *bias += delta_bias;
-        }
-        for (weight, delta_weight) in self.weights.iter_mut().zip(network.weights.iter()) {
-            *weight += delta_weight;
-        }
-    }
-
-    pub fn debug(&self) {
-        println!("{}", self.biases[1]);
     }
 
     fn sigmoid(x: f64) -> f64 {
@@ -155,38 +178,6 @@ mod tests {
         assert_eq!(
             network.feedforward(&Vector::new(vec![0.0, 0.0, 0.0])),
             Vector::new(vec![0.5, 0.5, 0.5, 0.5])
-        );
-    }
-
-    #[test]
-    fn test_train() {
-        let mut network = Network::new(&vec![1 as usize, 10 as usize, 4 as usize]);
-        let _network = Network::init(&vec![1 as usize, 10 as usize, 4 as usize]);
-        println!(
-            "Layer 0 -> Layer 1\n{}\n{}",
-            network.weights[0], network.biases[0]
-        );
-        println!(
-            "Layer 1 -> Layer 2\n{}\n{}",
-            network.weights[1], network.biases[1]
-        );
-        network.delta(&vector![1.0], &vector![1.0, 2.0, 3.0, 4.0], &_network);
-        println!(
-            "Layer 0 -> Layer 1\n{}\n{}",
-            network.weights[0], network.biases[0]
-        );
-        println!(
-            "Layer 1 -> Layer 2\n{}\n{}",
-            network.weights[1], network.biases[1]
-        );
-        network.delta(&vector![-1.0], &vector![-1.0, -2.0, -3.0, -4.0], &_network);
-        println!(
-            "Layer 0 -> Layer 1\n{}\n{}",
-            network.weights[0], network.biases[0]
-        );
-        println!(
-            "Layer 1 -> Layer 2\n{}\n{}",
-            network.weights[1], network.biases[1]
         );
     }
 }
